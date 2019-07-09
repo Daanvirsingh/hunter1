ae84604c22a40fabb09cce3ac128e9d6deebcd69
daanvir <daanvirsingh2@gmail.com>
2019-07-09 20:57:02 +0530

    jj

---
+++ hunter6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,k;
+    cin>>n;
+    int a[n];
+    cin>>k;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    cout<<a[n-k];
+    
+    return 0;
+}
