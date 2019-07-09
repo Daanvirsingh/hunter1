4744e14470d5154cf3a2c36416d139ee40516650
daanvir <daanvirsingh2@gmail.com>
2019-07-09 13:20:06 +0530

    kk

--- hunter.cpp
+++ hunter.cpp
@@ -1,21 +1,24 @@
-
 #include<iostream>
 using namespace std;
 
-
-int main(){
-    int n,m=0;
-    int a[n];
-    for(int i=0;i<n;i++){
-        if(m<a[i]){
-            m=a[i];
-        }
-    }
-    int c[m];
-    for(int i=0;i<n;i++){
-        c[a[i]]++;
-        if(c[a[i]]==2){
-            cout<<a[i]<<" ";
-        }
-    }
+main(){
+	long int arr[10000]={0};
+	long int n,e,flag=-1;
+	cin>>n;
+	for(long int i=0;i<n;i++){
+		cin>>e;
+		if(e<10000){
+			arr[e]++;
+		}
+	}
+	for(long int i=0;i<10000;i++){
+		if(arr[i]>1){
+			flag=0;
+			cout<<i<<" ";
+		}
+	}
+if(flag==-1){
+	cout<<"unique";
+}
+	
 }
