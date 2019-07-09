06c278c469a7ac6f3f6a4594209cab19d22da041
daanvir <daanvirsingh2@gmail.com>
2019-07-09 13:34:30 +0530

    l

--- hunter2.cpp
+++ hunter2.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 
 int main(){
-    int n=5;
-    int a[n]={1,8,3,4,7};
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
     sort(a,a+n);
     while(n--){
         cout<<a[n];
